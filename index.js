9d9308770927adf8afad206658e80f460aaca985
sravani <kunchasravani@gmail.com>
2025-03-27 22:36:53 +0530

    add socket programming

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors=require('cors');
 const cookieParser=require('cookie-parser');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
 
 const passport = require('passport');
 require("dotenv").config();
@@ -19,6 +21,10 @@ const postRouter=require('./routes/posts')
 const oAuthRouter=require('./routes/oauth')
 const jwtAuth=require('./middleware/auth.middleware');
 const refreshSignedUrls=require('./middleware/refreshSignedUrls.middleware');
+const chatRouter=require('./routes/chat.controller');
+const messageRouter=require('./routes/message.controller')
+const groupRouter=require('./routes/group.controller')
+const onlineUserModel=require('./models/onlineuser.model');
 
 // middleware
 app.use(express.json());
@@ -51,14 +57,70 @@ app.use("/api/search",jwtAuth,searchRouter);
 app.use("/api/alerts",jwtAuth,alertRouter);
 app.use('/api/posts',jwtAuth,postRouter);
 app.use('/auth', oAuthRouter);
+app.use('/api/chat',chatRouter);
+app.use('/api/messages',messageRouter);
+app.use('/api/groups',groupRouter);
+
+const server = http.createServer(app);
+const io = socketIo(server,{
+  cors: {
+     origin: '*'
+  }
+});
 
 //refresh signed Urls every 5mins
 setInterval(()=>{
   refreshSignedUrls()
 },process.env.REFRESH_SIGNED_URL)
 
+io.on('connection',(socket)=>{
+  console.log('cient connected '+socket.id);
+
+  socket.on('saveUser',async (loginData)=>{
+      loginData.loginProfileId && await onlineUserModel.findOneAndUpdate({loginProfileId:{$eq:loginData.loginProfileId}},loginData,{upsert: true});
+      try{
+        const socketData=await onlineUserModel.find({});
+        const socketIds=socketData.map((eachSocketData)=>eachSocketData.socket)
+        console.log("onlineusers",socketData)
+        io.to(socketIds).emit('onlineUsers', socketData);
+        }catch(err){
+          console.log(err);
+        }
+  });
+
+
+  socket.on('sendMessage',async(messageData)=>{
+    //get targetuserId socketId from onlineUsers if user is present.
+    const otherUserid=messageData.otherProfileId;
+    console.log(otherUserid);
+    const socketData=await onlineUserModel.find({loginProfileId:{ $in:otherUserid}});
+    if(socketData==null){
+      console.log("other userid offline");
+    }else{
+      const socketIds=socketData.map((eachSocketData)=>eachSocketData.socket)
+      console.log(socketIds)
+      io.to(socketIds).emit('getMessage', messageData.message);
+    }
+  })
+
+
+  socket.on('disconnect',async ()=>{
+    console.log('client disconnected '+socket.id);
+    const deleteRepsonse=await onlineUserModel.findOneAndDelete({socket:socket.id});
+    console.log('user went offline')
+  });
+
+ 
+})
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Running server on ${PORT}`);
-});
+server.listen(PORT,()=>{
+    console.log(`Running server on ${PORT}`);
+    // connectToMongoDB();
+})
+
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Running server on ${PORT}`);
+// });
---
+++ routes/chat.controller.js
@@ -0,0 +1,42 @@
+const express=require('express');
+
+const chatRouter=express.Router();
+const chatModel=require('../models/chat.model');
+const onlineUserModel=require('../models/onlineuser.model')
+//createChat when someone clicked on 
+chatRouter.post('/createChat',async(req,res)=>{
+    const {firstId,secondId}=req.body;
+
+    try{
+        let chatResponse=await chatModel.findOne({members:{$all:[firstId,secondId]}});
+        if(!chatResponse){
+            const chatReq={
+                members:[firstId,secondId]
+            }
+            chatResponse=new chatModel(chatReq);
+            await chatResponse.save();
+        }
+        res.status(200).send({"success":true,"message":"Chat created successfully","response":chatResponse});
+    }catch(err){
+        console.log(err);
+        res.status(500).send({"success":false,"message":"Error While creating chat","errorMsg":err.message});
+    }
+})
+
+//getusrchats -- when chats is clicked all existing chats which all ae there
+
+chatRouter.get('/getChats',async(req,res)=>{
+    const profileId=req.query.profileId;
+
+    try{
+        const chats=await chatModel.find({members:{$in:[profileId]}});
+        // console.log(chats)
+        res.status(200).send({"success":true,"message":"Chats Fetched successfully","response":chats});
+    }catch(err){
+        console.log(err);
+        res.status(500).send({"success":false,"message":"Error Getting Chats for user","errorMsg":err.message})
+    }   
+    
+})
+
+module.exports=chatRouter;
